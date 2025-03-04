cef901a54e5524584739364a4635140063b1119c
Mateusz Moskala <mmoskala07@gmail.com>
2025-03-04 21:08:51 +0100

    Style fix

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,28 +1,23 @@
 #pragma once
 #include <string>
 
-std::string calculate(const std::string& command, int first, int second) 
-{
+std::string calculate(const std::string& command, int first, int second) {
     std::string result;
 
-    if (command == "add")
-    {
-        result = std::to_string(first+second);
-    }
-    else if (command == "subtract")
-    {
-        result = std::to_string(first-second);
-    }
-    else if (command == "multiply")
-    {
-        result = std::to_string(first*second);
-    }
-    else if (command == "divide")
-    {
-        result = std::to_string(first/second);
-    }
-    else
-    {
+    if (command == "add") {
+        result = std::to_string(first + second);
+    } else if (command == "subtract") {
+        result = std::to_string(first - second);
+    } else if (command == "multiply") {
+        result = std::to_string(first * second);
+    } else if (command == "divide") {
+        if (second != 0) {
+            result = std::to_string(first / second);
+        }
+        else {
+            result = "Division by 0";
+        }
+    } else {
         result = "Invalid data";
     }
 
