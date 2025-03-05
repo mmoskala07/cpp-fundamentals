5d8a6b5ae919507b396dc9a01e87e449dc889c62
Mateusz Moskala <mmoskala07@gmail.com>
2025-03-05 15:13:26 +0100

    Implemented NWD and NWW

--- homework/nwd-nnw/nwdNww.hpp
+++ homework/nwd-nnw/nwdNww.hpp
@@ -1,11 +1,29 @@
 #pragma once
 
 int NWD(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    int result = 0;
+    lhs = std::abs(lhs);
+    rhs = std::abs(rhs);
+
+    if (lhs == 0 || rhs == 0) {
+        result = (rhs > lhs) ? rhs : lhs;
+    } else {
+        while (lhs != rhs) {
+            if (lhs > rhs) {
+                lhs -= rhs;
+            } else {
+                rhs -= lhs;
+            }
+        }
+        result = lhs;
+    }
+    return result;
 }
 
 int NWW(int lhs, int rhs) {
-    // TODO: Implement me :)
-    return -1;
+    if (lhs == 0 || rhs == 0) {
+        return 0;
+    } else {
+        return std::abs(lhs * rhs / NWD(lhs, rhs));
+    }
 }
