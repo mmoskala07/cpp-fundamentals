d1e28548be66060afcbe89507b3049a534443f4a
Mateusz Moskala <mmoskala07@gmail.com>
2025-03-05 09:52:24 +0100

    Added fibonacci implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,28 @@
 #pragma once
+#include <vector>
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result = 0;
+    std::vector<int> fibo = {0, 1};
+
+    if (sequence <= 1) {
+        result = fibo[sequence];
+    } else {
+        for (size_t i = 2; i <= sequence; i++) {
+            fibo.push_back(fibo[i - 1] + fibo[i - 2]);
+        }
+        result = fibo.back();
+    }
+
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0) {
+        return 0;
+    } else if (sequence == 1) {
+        return 1;
+    } else {
+        return fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2);
+    }
 }
