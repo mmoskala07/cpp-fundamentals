c37066af72d757fcb5cc12fe4ddb43bd9b4f9726
Mateusz Moskala <mmoskala07@gmail.com>
2025-03-06 22:03:49 +0100

    GenerateSequence implemented

--- homework/generate-sequence/generateSequence.hpp
+++ homework/generate-sequence/generateSequence.hpp
@@ -2,6 +2,14 @@
 #include <vector>
 
 std::vector<int> generateSequence(int count, int step) {
-    // TODO: Implement me :)
-    return {};
+    std::vector<int> vec;
+    int val = step;
+
+    if (count > 0) {
+        for (size_t i = 0; i < count; i++) {
+            vec.push_back(val);
+            val += step;
+        }
+    }
+    return vec;
 }
