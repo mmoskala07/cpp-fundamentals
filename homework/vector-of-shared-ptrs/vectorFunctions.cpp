6acbaed17184f92d762bafb0d6dc706a5bb68619
Mateusz Moskala <mmoskala07@gmail.com>
2025-03-06 18:03:52 +0100

    Style format

--- homework/vector-of-shared-ptrs/vectorFunctions.cpp
+++ homework/vector-of-shared-ptrs/vectorFunctions.cpp
@@ -1,52 +1,40 @@
-#include <iostream>
 #include "vectorFunctions.hpp"
+#include <iostream>
 
-std::vector<std::shared_ptr<int>> generate(int count)
-{
+std::vector<std::shared_ptr<int>> generate(int count) {
     std::vector<std::shared_ptr<int>> vec;
 
-    for (size_t i=0; i<count; i++)
-    {
+    for (size_t i = 0; i < count; i++) {
         std::shared_ptr<int> ptr = std::make_shared<int>(i);
         vec.push_back(ptr);
     }
     return vec;
 }
 
-void print(std::vector<std::shared_ptr<int>> vec)
-{
-    for (auto ptr : vec)
-    {
-        if (ptr.get() != nullptr)
-        {
+void print(std::vector<std::shared_ptr<int>> vec) {
+    for (auto ptr : vec) {
+        if (ptr.get() != nullptr) {
             std::cout << *ptr << std::endl;
         }
     }
 }
 
-void add10(std::vector<std::shared_ptr<int>> vec)
-{
-    for (auto ptr : vec)
-    {
-        if (ptr.get() != nullptr)
-        {
+void add10(std::vector<std::shared_ptr<int>> vec) {
+    for (auto ptr : vec) {
+        if (ptr.get() != nullptr) {
             *ptr += 10;
         }
     }
 }
 
-void sub10(int * const ptr)
-{
-    if (ptr != nullptr)
-    {
+void sub10(int* const ptr) {
+    if (ptr != nullptr) {
         *ptr -= 10;
     }
 }
 
-void sub10(std::vector<std::shared_ptr<int>> vec)
-{
-    for (auto ptr : vec)
-    {
+void sub10(std::vector<std::shared_ptr<int>> vec) {
+    for (auto ptr : vec) {
         sub10(ptr.get());
     }
 }
